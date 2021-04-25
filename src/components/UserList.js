4397ddba472bc5e2afa5fbcaa3d286c335faab27
Diego Oliveira Rodrigues <diegopso2@gmail.com>
2021-04-25 12:33:08 +0200

    Isolating subcomponents of UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -6,11 +6,50 @@ import { loadNextPage } from '../store/pagination/actions'
 import { upperCaseFirst } from '../helpers/string'
 import { ExclamationCircleTwoTone, LoadingOutlined } from '@ant-design/icons'
 
-const UserList = (props) => {
+const { Meta } = Card
+
+const User = ({ user }) => {
+  if (user.hide) {
+    return <></>
+  }
+
+  return (
+    <Col className="user-container" span="6">
+      <Card>
+        <Meta
+          avatar={<Avatar src={user.picture.thumbnail} />}
+          title={upperCaseFirst(user.name.first) + ' ' + upperCaseFirst(user.name.last)}
+          description={'@' + user.login.username}
+        />
+      </Card>
+    </Col>
+  )
+}
+
+const Loader = ({ error, onReattempt }) => {
+  if (!error) {
+    return (
+      <div className="loading-container">
+        <Spin tip="Loading..." indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}></Spin>
+      </div>
+    )
+  }
+
+  return (
+    <div className="loading-container">
+      <div>
+        <ExclamationCircleTwoTone className="error-icon" twoToneColor="red" />
+        <p>There was a problem loading the list of users.</p>
+        <Button type="primary" onClick={onReattempt}>Try Again</Button>
+      </div>
+    </div>
+  )
+}
+
+const UserList = () => {
   const pagination = useSelector((state) => state.pagination)
   const settings = useSelector((state) => state.settings)
   const dispatch = useDispatch()
-  const { Meta } = Card
 
   useEffect(() => {
     if (settings.length > 0 && !pagination.error && pagination.items.length === 0) {
@@ -22,44 +61,6 @@ const UserList = (props) => {
     if (!pagination.query && settings.length > 0 && !pagination.error) {
       dispatch(loadNextPage(pagination, settings))
     }
-  }
-
-  const User = (props) => {
-    if (props.user.hide) {
-      return ''
-    }
-
-    return (
-      <Col className="user-container" span="6">
-        <Card>
-          <Meta
-            avatar={<Avatar src={props.user.picture.thumbnail} />}
-            title={upperCaseFirst(props.user.name.first) + ' ' + upperCaseFirst(props.user.name.last)}
-            description={'@' + props.user.login.username}
-          />
-        </Card>
-      </Col>
-    )
-  }
-
-  const Loader = () => {
-    if (!pagination.error) {
-      return (
-        <div className="loading-container">
-          <Spin tip="Loading..." indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}></Spin>
-        </div>
-      )
-    }
-
-    return (
-      <div className="loading-container">
-        <div>
-          <ExclamationCircleTwoTone className="error-icon" twoToneColor="red" />
-          <p>There was a problem loading the list of users.</p>
-          <Button type="primary" onClick={() => { dispatch(loadNextPage(pagination, settings)) }}>Try Again</Button>
-        </div>
-      </div>
-    )
   }
 
   return (
@@ -78,7 +79,9 @@ const UserList = (props) => {
           dataLength={pagination.items.length}
           next={loadMore}
           hasMore={!pagination.query && pagination.page < pagination.maxPages}
-          loader={<Loader/>}
+          loader={
+            <Loader onReattempt={() => { dispatch(loadNextPage(pagination, settings)) }} error={pagination.error} />
+          }
           endMessage={
             <Divider orientation="center">
               { pagination.query ? 'Pagination temporarily disabled' : 'End of users catalog' }
